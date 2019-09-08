6eef98d52189057c29accba051e024fd7092c20b
Fettes <396511673@qq.com>
2019-09-07 20:38:52 -0400

    x

--- src/socketExercise/Escape_modified.py
+++ src/socketExercise/Escape_modified.py
@@ -294,19 +294,20 @@ def main(args):
     # Client test
     s = socket.socket()
     # connect the server
-    s.connect(('192.168.56.1', 2048))  # ①
+    s.connect(('192.168.200.52', 19002))  # ①
 
     # Define the command list
-    # command_list = ["Tianshi Feng", "look", "look chest", "look mirror", "get hairpin",
-    #                 "unlock chest with hairpin", "open chest", "look in chest", "unlock door with hairpin",
-    #                 "open door"]
-    # for i in command_list:
-    #     res = s.recv(1024)
-    #     print(res.decode('utf-8'))
-    #     s.send(i.encode('utf-8'))
-    #     time.sleep(0.25)
-    # res_suc = s.recv(1024)
-    # print(res_suc.decode('utf-8'))
+    command_list = ["Tianshi Feng", "look", "look chest", "look mirror", "get hairpin",
+                    "unlock chest with hairpin", "open chest", "look in chest", "unlock door with hairpin",
+                    "open door"]
+    for i in command_list:
+        res = s.recv(1024)
+        print(res.decode('utf-8'))
+        s.send(i.encode('utf-8'))
+        time.sleep(0.25)
+    res_suc = s.recv(1024)
+    print(res_suc.decode('utf-8'))
+
     # Server test
     def send_message(result):
         s.send(result.encode('utf-8'))
